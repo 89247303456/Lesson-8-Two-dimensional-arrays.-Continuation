304267ffe850aef1d8f13fce080274c82f27f2fe
DmitriiLZUTKM <89247303456@mail.ru>
2023-08-05 15:03:33 +1000

    first commit

---
+++ Task 54/Program.cs
@@ -0,0 +1,116 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] array = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 }
+        };
+
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        Console.WriteLine("Исходный массив:");
+
+        // Вывод исходного массива
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+
+        // Упорядочивание элементов каждой строки по убыванию
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns - 1; j++)
+            {
+                // Находим максимальный элемент в оставшейся части строки
+                int maxIndex = j;
+                for (int k = j + 1; k < columns; k++)
+                {
+                    if (array[i, k] > array[i, maxIndex])
+                    {
+                        maxIndex = k;
+                    }
+                }
+
+                // Меняем местами текущий элемент и максимальный элемент
+                int temp = array[i, j];
+                array[i, j] = array[i, maxIndex];
+                array[i, maxIndex] = temp;
+            }
+        }
+
+        Console.WriteLine("Упорядоченный массив:");
+
+        // Вывод упорядоченного массива
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+// int[,] array = {
+//             { 1, 4, 7, 2 },
+//             { 5, 9, 2, 3 },
+//             { 8, 4, 2, 4 }
+//         };
+
+//         int rows = array.GetLength(0);
+//         int columns = array.GetLength(1);
+
+//         Console.WriteLine("Исходный массив:");
+
+//         // Вывод исходного массива
+//         for (int i = 0; i < rows; i++)
+//         {
+//             for (int j = 0; j < columns; j++)
+//             {
+//                 Console.Write($"{array[i, j]} ");
+//             }
+//             Console.WriteLine();
+//         }
+
+//         // Упорядочивание элементов каждой строки по убыванию
+//         for (int i = 0; i < rows; i++)
+//         {
+//             Array.Sort(array, i * columns, columns, new ReverseComparer());
+//         }
+
+//         Console.WriteLine("Упорядоченный массив:");
+
+//         // Вывод упорядоченного массива
+//         for (int i = 0; i < rows; i++)
+//         {
+//             for (int j = 0; j < columns; j++)
+//             {
+//                 Console.Write($"{array[i, j]} ");
+//             }
+//             Console.WriteLine();
+//         }
+
+//         // Реверсивный компаратор для сортировки по убыванию
+// class ReverseComparer :IComparer
+// {
+//     public int Compare(object x, object y)
+//     {
+//         return ((IComparable)y).CompareTo(x);
+//     }
+// }
---
+++ Task 62/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.WriteLine("Введите размер массива (например, 4):");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[,] array = new int[size, size];
+        int num = 1;
+        int startRow = 0;
+        int endRow = size - 1;
+        int startCol = 0;
+        int endCol = size - 1;
+
+        while (num <= size * size)
+        {
+            for (int i = startCol; i <= endCol; i++) // Заполняем верхнюю строку слева направо
+            {
+                array[startRow, i] = num++;
+            }
+            startRow++;
+
+            for (int i = startRow; i <= endRow; i++) // Заполняем правый столбец сверху вниз
+            {
+                array[i, endCol] = num++;
+            }
+            endCol--;
+
+            for (int i = endCol; i >= startCol; i--) // Заполняем нижнюю строку справа налево
+            {
+                array[endRow, i] = num++;
+            }
+            endRow--;
+
+            for (int i = endRow; i >= startRow; i--) // Заполняем левый столбец снизу вверх
+            {
+                array[i, startCol] = num++;
+            }
+            startCol++;
+        }
+
+        // Выводим заполненный массив
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write("{0, 2} ", array[i, j]);
+            }
+            Console.WriteLine();
+        }
